1238eacbe2d7b8ce47022cd5d4cbcede838f1d12
Admin <Admin@DESKTOP-GK1B207>
2023-08-30 15:04:13 +0530

    Add project files.

---
+++ Student_CrudOperation/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student_CrudOperation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int no = 0;
+            Studentcrud crud=new Studentcrud();
+            do
+            {
+                Console.WriteLine("1.Enter student list");
+                Console.WriteLine("2.Get Student By Roll Number");
+                Console.WriteLine("3.Add Student");
+                Console.WriteLine("4.Update Student");
+                Console.WriteLine("5.Delete Student");
+
+                Console.WriteLine("Select option");
+                int option=Convert.ToInt32(Console.ReadLine());
+
+                switch (option)
+                {
+
+                    case 1:
+                        List<Student> list = crud.GetStudent();
+                        Console.WriteLine("RollNo,Name,Percentage,ClgName");
+                        foreach (Student student in list)
+                        {
+
+                            Console.WriteLine($"{student.rollno},{student.Name},{student.percentage},{student.clgname}");
+                        }
+                        break;
+                    case 2:
+                        Console.WriteLine("Enter student Rollno");
+                        int rollno=Convert.ToInt32(Console.ReadLine());
+                        Student s=crud.GetStudentByRollNo(rollno);
+                        Console.WriteLine("RollNo,Name,Percentage,ClgName");
+                        Console.WriteLine($"{s.rollno},{s.Name},{s.percentage},{s.clgname}");
+                        break;
+                    case 3:
+                        Student s1 = new Student();
+                        Console.WriteLine("Enter student Rollno you want to ADD");
+                        s1.rollno=Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enter student Name");
+                        s1.Name=Console.ReadLine();
+                        Console.WriteLine("Enter Student percentage");
+                        s1.percentage=Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("Enter clg name");
+                        s1.clgname=Console.ReadLine();
+                        crud.AddStudent(s1);
+                        Console.WriteLine("Employee Details saved");
+                        break;
+                    case 4:
+                        Student s2 = new Student();
+                        Console.WriteLine("Enter student Rollno you want to update");
+                        s2.rollno = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enter student Name");
+                        s2.Name = Console.ReadLine();
+                        Console.WriteLine("Enter Student percentage");
+                        s2.percentage = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("Enter clg name");
+                        s2.clgname = Console.ReadLine();
+                        crud.UpdateStudent(s2);
+                        Console.WriteLine("Employee Details Updated");
+                        break;
+                    case 5:
+                        Console.WriteLine("Enter rollno you want to delete");
+                        int rollno1=Convert.ToInt32(Console.ReadLine());
+                        crud.DeleteStudent(rollno1);
+                        Console.WriteLine($"{rollno1} Deleted");
+                        break;
+
+                
+                }
+                Console.WriteLine("Press 1 for continue..");
+                no=Convert.ToInt32(Console.ReadLine());
+
+            }
+            while (no == 1);
+
+
+
+
+        }
+    }
+}
---
+++ Student_CrudOperation/Student.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student_CrudOperation
+{
+    public class Student
+    {
+        public int rollno { get; set; }
+        public string Name { get; set; }
+        public double percentage { get; set; }
+
+        public string clgname { get; set; }
+
+
+    }
+    public class Studentcrud
+    {
+        private List<Student> studentlist;
+        public Studentcrud()
+        {
+            studentlist = new List<Student>()
+            {
+                new Student{ rollno=111  ,Name="Aditya",percentage=95.20,clgname="Dy patil"},
+                new Student{ rollno=112  ,Name="Rutuja",percentage=85.30,clgname="bharati vidyapeeth"}
+
+            };
+        
+        }
+        public List<Student> GetStudent()
+        { 
+            return studentlist;
+
+        
+        }
+        public Student GetStudentByRollNo(int rollno)
+        {
+            Student student1 = new Student();
+            foreach (Student s in studentlist)
+            {
+                if (s.rollno == rollno)
+                { 
+                
+                    student1= s;
+                    break;
+                
+                }
+            }
+            return student1;
+        
+        }
+        public void AddStudent(Student s)
+        { 
+         studentlist.Add(s);
+        
+        }
+        public void UpdateStudent(Student s)
+        { 
+            foreach(Student student in studentlist) 
+            {
+                if (student.rollno == s.rollno)
+                {
+                    student.Name = s.Name;
+                    student.percentage = s.percentage;
+                    student.clgname = s.clgname;
+                    break;
+                }
+            
+            }
+
+        
+        }
+        public void DeleteStudent(int rollno)
+        {
+            foreach (Student student1 in studentlist)
+            {
+
+                if (student1.rollno == rollno)
+                { 
+                
+                studentlist.Remove(student1);
+                    break;
+                
+                }
+            
+            
+            
+            }
+
+        
+        
+        }
+    
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
+    }
+
+}
